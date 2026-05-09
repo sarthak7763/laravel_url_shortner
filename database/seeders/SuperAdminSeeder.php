8aa9631a6620cb704f112b6daa927da46e629f52
Sarthak Agrawal <sarthakagrawal961@gmail.com>
2026-05-09 14:41:13 +0530

    Initial Laravel project setup

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        $user = User::firstOrCreate(
+            ['email' => 'superadmin@example.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $user->assignRole('SuperAdmin');
+    }
+}
